b673c699084d726982b6e64cd8304f68413b2334
Vass9811 <33204122+Vass9811@users.noreply.github.com>
2022-03-15 13:47:08 +0200

    Initial commit

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  template:`
+  <p>
+  hello-world works inline!
+  </p>`,
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
